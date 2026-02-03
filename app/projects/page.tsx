4233e0b3f807fe2f40627967219a80a1675b1fd7
ilham saleh <ilham2saleh@gmail.com>
2026-02-03 19:38:35 +0000

    Learn about dmetadata

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,22 +1,22 @@
-import Projects from "app/projects/projects";
-import { Fragment } from "react";
-import Header from "../components/header";
+// import Projects from "app/projects/projects";
+// import { Fragment } from "react";
+// import Header from "../components/header";
 
-// export const metadata = {
-//   title: "Projects",
-//   description: "My Projects - Ilham Saleh",
-// };
+// // export const metadata = {
+// //   title: "Projects",
+// //   description: "My Projects - Ilham Saleh",
+// // };
 
-export default function Page() {
-  return (
-    <Fragment>
-      <Header title="Projects" />
-      <div className="space-y-2 md:space-y-5">
-        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-          Here are some of my selected projects worth sharing.
-        </p>
-      </div>
-      <Projects />
-    </Fragment>
-  );
-}
+// export default function Page() {
+//   return (
+//     <Fragment>
+//       <Header title="Projects" />
+//       <div className="space-y-2 md:space-y-5">
+//         <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+//           Here are some of my selected projects worth sharing.
+//         </p>
+//       </div>
+//       <Projects />
+//     </Fragment>
+//   );
+// }
