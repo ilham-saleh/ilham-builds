96c65834af3515368afaf7c30fdb54a1c84e12a5
ilham saleh <ilham2saleh@gmail.com>
2026-03-21 19:42:39 +0000

    sasa

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,17 +1,17 @@
-import { ClipPathLinks } from "./mega-ui/links/ClipPathLinks";
+// import { ClipPathLinks } from "./mega-ui/links/ClipPathLinks";
 
-export default function Contact() {
-  return (
-    <section className="relative h-screen w-screen py-10 px-12 md:px-32 xl:px-36 dark:bg-black dark:text-white bg-white text-black">
-      <div className="flex flex-col justify-evenly h-5/6">
-        {/* <span className="text-3xl md:text-6xl xl:text-8xl">
-          Let&apos;s make something <br></br> valuable together
-        </span>
-        <span className="text-3xl md:text-6xl xl:text-8xl text-end">
-          <span className="underline">ilham2saleh@gmail.com</span>
-        </span> */}
-        <ClipPathLinks />
-      </div>
-    </section>
-  );
-}
+// export default function Contact() {
+//   return (
+//     <section className="relative h-screen w-screen py-10 px-12 md:px-32 xl:px-36 dark:bg-black dark:text-white bg-white text-black">
+//       <div className="flex flex-col justify-evenly h-5/6">
+//         {/* <span className="text-3xl md:text-6xl xl:text-8xl">
+//           Let&apos;s make something <br></br> valuable together
+//         </span>
+//         <span className="text-3xl md:text-6xl xl:text-8xl text-end">
+//           <span className="underline">ilham2saleh@gmail.com</span>
+//         </span> */}
+//         <ClipPathLinks />
+//       </div>
+//     </section>
+//   );
+// }
